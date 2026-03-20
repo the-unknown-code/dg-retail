251ec6650f38d7eb14165160d218fd0ca4637974
the-unknown-code <hello@unk.digital>
2026-03-20 22:32:15 +0700

    Update index.ts

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,8 +10,8 @@ function createWindow(): void {
   app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required')
 
   const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 1920,
+    height: 1080,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
