8643f0438995c56083478a0d6648fa6811b34259
the-unknown-code <hello@unk.digital>
2026-03-23 17:59:31 +0700

    Update index.ts

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -57,7 +57,7 @@ export const useAppStore = defineStore('app', {
       BL: 0,
       BR: 0
     } as { TL: number; TR: number; BL: number; BR: number },
-    playDuration: getQueryParam('debug') === '1' ? 15 : 20,
+    playDuration: getQueryParam('debug') === '1' ? 15 : 40,
     qrDuration: getQueryParam('debug') === '1' ? 15 : 15,
     tweakpane: getQueryParam('debug') === '1' ? new Pane() : null,
     midiData: {
