1e53a89cafa2eefeeef40896a08457095a6305c0
the-unknown-code <hello@unk.digital>
2026-03-30 22:38:58 +0700

    Update jogwheel.vue

--- src/renderer/src/components/ui/jogwheel.vue
+++ src/renderer/src/components/ui/jogwheel.vue
@@ -236,7 +236,7 @@ const applySpeed = (speed: number): void => {
     pinState.vy = Math.abs(speed) / 10
   }
 
-  pinState.y = clamp(pinState.y, -bounds.y, 45)
+  pinState.y = clamp(pinState.y, -bounds.y, -10)
 
   pinState.nx = pinState.x / bounds.x
   pinState.ny = pinState.y / bounds.y
